bc526a838ecab11ef70a4eb20b49c23befe24aee
eric.song <eric.song@theminesec.com>
2024-11-04 16:12:41 +0800

    updated package id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.msa.headless"
+        applicationId = "com.msa.headless.bctc"
         minSdk = 26
         targetSdk = 34
         /**
@@ -42,7 +42,7 @@ android {
         // DO NOT CHANGE, re-cert require
         val minorVersion = 0
         // msa component lib version
-        val patchVersion: Int = "003".toInt()
+        val patchVersion: Int = "005".toInt()
         // license update, build config changes
         val buildVersion = 1
         // Neurogine
@@ -161,7 +161,7 @@ dependencies {
     implementation(libs.signaturepad)
 
     //implementation("com.theminesec.sdk:headless-stage:1.0.17")
-//    implementation("com.theminesec.sdk:headless-mpoc-stage:2.0.1")
-    implementation("com.theminesec.sdk:headless-mpoc:2.0.1")
+    implementation("com.theminesec.sdk:headless-mpoc-stage:2.0.1")
+//    implementation("com.theminesec.sdk:headless-mpoc:2.0.1")
 
 }
